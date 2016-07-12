85fea3a568fbc2f03f7167575ea77993d91b4c53
kherel <kherel@users.noreply.github.com>
2016-07-12 15:39:37 +0700

    fix syntax in server api

--- simple_api/api/index.js
+++ simple_api/api/index.js
@@ -6,20 +6,20 @@ const assign = require('object-assign');
 const uuid = require('node-uuid');
 
 router.get('/employees', function (req, res, next) {
-    var employees = mocks.employees;
+    const employees = mocks.employees;
     if (employees) return res.json(employees);
     res.status(404).json({error: "not found"});
 });
 
 router.post('/employees', function (req, res, next) {
-    var body = req.body;
+    const body = req.body;
     if(body){
-        var employee = {
+        const employee = {
             id: uuid.v4(),
             name: body.name,
             email: body.email
         };
-        var employees = mocks.employees;
+        const employees = mocks.employees;
         employees.push(employee);
         res.json(employees)
     } else {
@@ -28,8 +28,8 @@ router.post('/employees', function (req, res, next) {
 });
 
 router.delete('/employees/:id', function (req, res, next) {
-    var employees = mocks.employees;
-    var employee;
+    const employees = mocks.employees;
+    let employee;
     for (let i = 0; i < employees.length; i++) {
         if(employees[i].id == req.params.id) {
             employee = employees.splice( i, 1 )[0];
